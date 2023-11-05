80fd6ffc8e26f4cd6a2a673c8f585d079ec0a876
Rahiful Islam <rahifulislam@gmail.com>
2023-11-06 01:33:55 +0600

    first commit

--- frontend-development-task/src/components/SignUp.jsx
+++ frontend-development-task/src/components/SignUp.jsx
@@ -3,9 +3,14 @@ import React from "react";
 const SignUp = () => {
   return (
     <div className="w-1440">
-      <div className="flex justify-between bg-red-500">
-        <div className="bg-red-500 h-4 w-6">Sign Up</div>
-        <div className="bg-blue-500 h-4 w-6">Sign In</div>
+      <div className="flex justify-between">
+        <div className="flex items-center gap-3">
+          {/* <div className="bg-gray-200">
+          </div> */}
+          <img className="h-[44px] w-[50px]" src="/icons/paper 1.png" alt="" />
+          <p className="text-[#4E5D78] font-[Inter] text-[28px] font-[700]">Stack</p>
+        </div>
+        <div className="bg-blue-500 h-4 w-6"></div>
       </div>
     </div>
   );
