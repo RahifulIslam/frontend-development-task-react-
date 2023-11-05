386fc661f4af9576f4527ad627c220e02cfeaaac
Rahiful Islam <rahifulislam@gmail.com>
2023-11-05 10:17:15 +0600

    first commit

---
+++ frontend-development-task/src/components/SignUp.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const SignUp = () => {
+  return (
+    <div className="w-1440">
+      <div className="flex justify-between bg-red-500">
+        <div className="bg-red-500 h-4 w-6">Sign Up</div>
+        <div className="bg-blue-500 h-4 w-6">Sign In</div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
